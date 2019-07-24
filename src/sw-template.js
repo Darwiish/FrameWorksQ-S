4e5eb8e1868fe6ecec6db35477abb9170e520979
abc <abc@example.com>
2019-07-24 17:00:41 +0200

    pac.json

--- src/sw-template.js
+++ src/sw-template.js
@@ -1,33 +1,48 @@
 if ('function' === typeof importScripts) {
-    importScripts(
+  importScripts(
       'https://storage.googleapis.com/workbox-cdn/releases/3.5.0/workbox-sw.js'
-    );
-    /* global workbox */
-    if (workbox) {
+  );
+  /* global workbox */
+  if (workbox) {
       console.log('Workbox is loaded');
-   
+
       /* injection point for manifest files.  */
       workbox.precaching.precacheAndRoute([]);
-   
-  /* custom cache rules*/
-  workbox.routing.registerNavigationRoute('/index.html', {
-        blacklist: [/^\/_/, /\/[^\/]+\.[^\/]+$/],
+
+      /* custom cache rules*/
+      workbox.routing.registerNavigationRoute('/index.html', {
+          blacklist: [/^\/_/, /\/[^\/]+\.[^\/]+$/],
       });
-   
-  workbox.routing.registerRoute(
-        /\.(?:png|gif|jpg|jpeg)$/,
-        workbox.strategies.cacheFirst({
-          cacheName: 'images',
-          plugins: [
-            new workbox.expiration.Plugin({
-              maxEntries: 60,
-              maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-            }),
-          ],
-        })
+
+      // Cache first strategy
+      workbox.routing.registerRoute(
+          /\.(?:png|gif|jpg|jpeg)$/,
+          workbox.strategies.cacheFirst({
+              cacheName: 'images',
+              plugins: [
+                  new workbox.expiration.Plugin({
+                      maxEntries: 60,
+                      maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+                  }),
+              ],
+          })
       );
-   
+
+      // Background sync 
+      const bgSyncPlugin = new workbox.backgroundSync.Plugin('Questionnaire Background Sync', {
+          maxRetentionTime: 24 * 60 // Retry for max of 24 Hours
+      });
+
+      // have a look at this https://developers.google.com/web/tools/workbox/modules/workbox-background-sync
+      workbox.routing.registerRoute(
+         "/api/answers",
+          new workbox.strategies.NetworkOnly({
+              plugins: [bgSyncPlugin]
+          }),
+          'POST'
+      );
+
   } else {
       console.log('Workbox could not be loaded. No Offline support');
-    }
   }
+}
