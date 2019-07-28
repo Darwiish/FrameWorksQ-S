27e8b0a963e51c6691c7a75a4043aca34f423c38
abc <abc@example.com>
2019-07-29 00:26:52 +0200

    aaa

--- src/sw-template.js
+++ src/sw-template.js
@@ -1,59 +1,56 @@
-if ('function' === typeof importScripts) {
+if ("function" === typeof importScripts) {
   importScripts(
-      'https://storage.googleapis.com/workbox-cdn/releases/3.5.0/workbox-sw.js'
+    "https://storage.googleapis.com/workbox-cdn/releases/3.5.0/workbox-sw.js"
   );
   /* global workbox */
   if (workbox) {
-      console.log('Workbox is loaded');
+    console.log("Workbox is loaded");
 
-      /* injection point for manifest files.  */
-      workbox.precaching.precacheAndRoute([]);
+    /* injection point for manifest files.  */
+    workbox.precaching.precacheAndRoute([]);
 
-      /* custom cache rules*/
-      workbox.routing.registerNavigationRoute('/index.html', {
-          blacklist: [/^\/_/, /\/[^\/]+\.[^\/]+$/],
-      });
+    /* custom cache rules*/
+    workbox.routing.registerNavigationRoute("/index.html", {
+      blacklist: [/^\/_/, /\/[^\/]+\.[^\/]+$/]
+    });
 
-      // Cache first strategy
-      workbox.routing.registerRoute(
-          /\.(?:png|gif|jpg|jpeg)$/,
-          workbox.strategies.cacheFirst({
-              cacheName: 'files',
-              plugins: [
-                  new workbox.expiration.Plugin({
-                      maxEntries: 60,
-                      maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-                  }),
-              ],
+    // Cache first strategy
+    workbox.routing.registerRoute(
+      /\.(?:png|gif|jpg|jpeg)$/,
+      workbox.strategies.cacheFirst({
+        cacheName: "files",
+        plugins: [
+          new workbox.expiration.Plugin({
+            maxEntries: 60,
+            maxAgeSeconds: 30 * 24 * 60 * 60 // 30 Days
           })
-      );
+        ]
+      })
+    );
 
-      workbox.routing.registerRoute(
-        "/api/(.*)",
-        workbox.strategies.cacheFirst({
-          cacheName: "questions",
-          cacheExpiration: {
-            maxEntries: 30
-          },
-          cacheableResponse: { statuses: [0, 200] }
-        })
-      );
+    workbox.routing.registerRoute(
+      "/api/(.*)",
+      workbox.strategies.cacheFirst({
+        cacheName: "questions",
+        cacheableResponse: { statuses: [0, 200] }
+      }),
+      "GET"
+    );
 
-      // Background sync 
-      const bgSyncPlugin = new workbox.backgroundSync.Plugin('Background Sync', {
-          maxRetentionTime: 24 * 60 // Retry for max of 24 Hours
-      });
+    // Background sync
+    const bgSyncPlugin = new workbox.backgroundSync.Plugin("Background Sync", {
+      maxRetentionTime: 24 * 60 // Retry for max of 24 Hours
+    });
 
-      // have a look at this https://developers.google.com/web/tools/workbox/modules/workbox-background-sync
-      workbox.routing.registerRoute(
-         "/api/questions",
-          new workbox.strategies.NetworkOnly({
-              plugins: [bgSyncPlugin]
-          }),
-          'POST'
-      );
-
+    // have a look at this https://developers.google.com/web/tools/workbox/modules/workbox-background-sync
+    workbox.routing.registerRoute(
+      "/api/questions",
+      new workbox.strategies.NetworkOnly({
+        plugins: [bgSyncPlugin]
+      }),
+      "POST"
+    );
   } else {
-      console.log('Workbox could not be loaded. No Offline support');
+    console.log("Workbox could not be loaded. No Offline support");
   }
 }
